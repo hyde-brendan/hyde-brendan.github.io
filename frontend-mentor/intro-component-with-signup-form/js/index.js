20ae0a67a1d92055897c1c57c49aea1fab508b21
Brendan Hyde <42706221+hyde-brendan@users.noreply.github.com>
2022-02-07 19:47:31 +0800

    Minor fixes

--- frontend-mentor/intro-component-with-signup-form/js/index.js
+++ frontend-mentor/intro-component-with-signup-form/js/index.js
@@ -50,7 +50,7 @@ function validateEmail(address) {
         Regex provided by https://emailregex.com/.
     `
     const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regex.test( String(address).toLowerCase() );
+    return regex.test( String(address.trim()).toLowerCase() );
 }
 
 function addInvalid(el) {
